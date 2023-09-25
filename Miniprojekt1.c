92d3d2df82060d96514a510c7305e3c5ef79c5a5
Lisa <85162235+LisNis@users.noreply.github.com>
2023-09-25 13:47:52 +0200

    Update Miniprojekt1.c

--- Miniprojekt1.c
+++ Miniprojekt1.c
@@ -1,1 +1,42 @@
-/*Akkumulator:*/
+#include <stdio.h>
+#include <math.h>
+#include "calculator.h"
+
+
+int main(void) {
+    char operator;
+    double inputNumber;
+    double result = 0;
+
+    printf("Resultetet er = %lf > ", result);
+
+    do {
+        scanf(" %c %lf", &operator, &inputNumber);
+
+        if(operator == '+'){
+            result = result + inputNumber;
+            printf("The result is %lf > ", result);
+        }else if(operator == '-'){
+            result = result - inputNumber;
+            printf("The result is %lf > ", result);
+        }else if(operator == '*'){
+            result = result * inputNumber;
+            printf("The result is %lf > ", result);
+        }else if(operator == '/') {
+            result = result / inputNumber;
+            printf("The result is %lf > ", result);
+        }else if(operator == '#') {
+            result = sqrt(result);
+            printf("The result is %lf > ", result);
+        }else if(operator == '%') {
+            result = result * -1;
+            printf("The result is %lf > ", result);
+        }else if(operator == '!') {
+            result = 1 / result;
+            printf("The result is %lf > ", result);
+        }
+
+    } while (operator != 'q');
+      
+    return 0;  
+}
