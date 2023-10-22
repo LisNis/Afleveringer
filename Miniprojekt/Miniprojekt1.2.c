cce2087aee2fb088b3853f9af8b936b3c959a417
Lisa <85162235+LisNis@users.noreply.github.com>
2023-10-22 16:00:07 +0200

    Update Miniprojekt1.2.c

--- Miniprojekt/Miniprojekt1.2.c
+++ Miniprojekt/Miniprojekt1.2.c
@@ -10,11 +10,22 @@ q for at afslutte regnemaskinen med slutresultatet
 #include <math.h>
 #include <stdlib.h>
 
+double calculator(double result, char operator,double tal);
+double calculateUnaereOperationer(double result, char operator);
+double calculateBinaereOperationer(double result, char operator, double tal);
+
 int main() {
     double result = 0.0;
     char operator;
     double tal;
 
+    calculator(result, operator, tal);
+
+    return 0;
+} // end main
+
+
+double calculator(double result, char operator, double tal){
     printf("Result er: %.2lf\n", result);
 
     while (1) {
@@ -25,49 +36,51 @@ int main() {
             printf("Programmet blev afsluttet.\n");
             break;
         }
-
+        
         if (operator == 33 || operator == 35 || operator == 37){
-            switch (operator) {
-                case '!':
-                    result = 1 / result;
-                    break;
-                case '%':
-                    result = result * -1;
-                    break;
-                case '#':
-                    result = sqrt(result);
-                    break;
-            }
+            result = calculateUnaereOperationer(result, operator);
         } else if (operator == 43 || operator == 45 || operator == 42 || operator == 92 || operator == 94) {
             scanf("%lf", &tal);
-
-            switch (operator) {
-                case '+':
-                    result = result + tal;
-                    break;
-                case '-':
-                    result = result - tal;
-                    break;
-                case '*':
-                    result = result * tal;
-                    break;
-                case '^':
-                    result = pow(result, tal);
-                    break;
-                case '/':
-                    if (tal != 0) {
-                        result = result / tal;
-                    } else {
-                        printf("Man kan ikke dividere med 0.\n");
-                    }
-                    break;
-            }// end switch
+            result = calculateBinaereOperationer(result, operator, tal);
         } else {
             printf("Operatoren findes ikke, indtast +, -, *, /, eller q for at afslutte programmet.\n");
         } // end if/else
 
         printf("Det nye resultat: %.2lf\n", result);
     }// end while
+} // end function
 
-    return 0;
-} // end main
+double calculateUnaereOperationer(double result, char operator) {
+    switch (operator) {
+        case '!':
+            return 1 / result;
+        case '%':
+            return result * -1;
+        case '#':
+            return sqrt(result);
+        default:
+            return result;
+    } // end switch
+} // end function
+
+double calculateBinaereOperationer(double result, char operator, double tal) {
+    switch (operator) {
+        case '+':
+            return result + tal;
+        case '-':
+            return result - tal;
+        case '*':
+            return result * tal;
+        case '^':
+            return pow(result, tal);
+        case '/':
+            if (tal != 0) {
+                return result / tal;
+            } else {
+                printf("Man kan ikke dividere med 0.\n");
+                return result;
+            }
+        default:
+            return result;
+    }// end switch
+} // end function
