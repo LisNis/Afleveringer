ff7fbc780bbe4b844fe1de9667084ce42b0491be
Lisa <85162235+LisNis@users.noreply.github.com>
2023-10-23 19:33:53 +0200

    Update and rename afleveringsopgave3.c to afleveringsopgave3.0.c

--- Aflevering/afleveringsopgave3.0.c
+++ Aflevering/afleveringsopgave3.0.c
@@ -5,12 +5,12 @@
 #include <math.h>
 
 double f(double x);
-double sigma(int n, double a, double b);
-double trap(int n, double a, double b);
+double sigma(int n, double a, double b, double h, double x);
+double trap(int n, double a, double b, double h, double x);
 
 
 int main() {
-    double a, b;
+    double a, b, h, x;
     int n;
 
     printf("Enter the interval a, b: ");
@@ -24,7 +24,7 @@ int main() {
         return 1;
     }
 
-    double result = trap(n, a, b);
+    double result = trap(n, a, b, h, x);
 
     printf("Arealet er: %.6lf\n", result);
 
@@ -39,26 +39,25 @@ double f(double x) {
     } else {
         return 0;
     } // a:-2, b:2, ellers nan
-}
+} // end function
 
 
 // beregner summen af funktionen
-double sigma(int n, double a, double b){
-    double h = (b - a) / n;
+double sigma(int n, double a, double b, double h, double x){
+    h = (b - a) / n;
     printf("H er %lf\n", h);
     double sum = 0;
     for (int i = 1; i < n; i++){
-        double x = a + i * h;
+        x = a + i * h;
         sum = sum + f(x);
     }
     printf("Summen er: %lf\n", sum);
     return sum;
-}
+} // end function
 
 // beregner arealet
-double trap(int n, double a, double b) {
-    double h = (b - a) / n;
-    double sum = sigma(n, a, b);
+double trap(int n, double a, double b, double h, double x) {
+    double sum = sigma(n, a, b, h, x);
     double areal = h / 2 * (f(a) + 2 * sum + f(b));
     return areal;
-}
+} // end function
